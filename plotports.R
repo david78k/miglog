1e20a40170608969d1331f91885449c7499a6627
root <root@rapidmigrate.com>
2014-06-24 01:12:32 -0400

    all

--- plotports.R
+++ plotports.R
@@ -135,7 +135,7 @@ genplot <- function (type) {
         	      legend.title = element_text(size = 14, face = 'bold'),
         	      legend.text = element_text(size = 12),
 		      #panel.margin = unit(1, "cm"),
-		      plot.margin = unit(c(1,1,1,1), "cm"),
+		      plot.margin = unit(c(1,1,0,0), "cm"),
         	      plot.title = element_text(size = 18, face = 'bold'))
 	plot(graph)
 
