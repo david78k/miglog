68e7fb7e7cf3d630b2954e7cfe0295c68b685e97
root <root@rapidmigrate.com>
2014-06-28 22:58:24 -0400

    all

--- plot/plotdensity.R
+++ plot/plotdensity.R
@@ -3,7 +3,8 @@
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 src <- args[1]
-prefix = src
+#prefix = src
+prefix = paste(src, sep = ".", "dens")
 
 xlabel = "THROUGHPUT (MB/S)"
 ylabel = "DENSITY"
@@ -54,7 +55,7 @@ genplot <- function (type) {
 	# c(bottom, left, top, right)
 	par(mar = c(5, 5, 1, 1) + 0.1)
 
-	plot(density(data))	
+	plot(density(data[,1]))	
 
 #	df <- data.frame(values = c(data[1,8], data[1:2,7], data[1:3,6], data[1:4,5], data[1:5,4], data[1:6,3], data[1:7,2], data[1:8,1]), 
 #		vars = rep(c("1", "2", "3", "4", "5", "6", "7", "8"), times = c(1,2,3,4,5,6,7,8)))
