d50e5656bc54eb79ca023bef2dafccaf48d6db6e
root <root@rapidmigrate.com>
2014-07-01 10:35:32 -0400

    all

--- plot/plotthroughput.R
+++ plot/plotthroughput.R
@@ -9,6 +9,7 @@ xlabel = "VM WINDOW"
 ylabel = "LINK UTILIZATION (%)"
 #ylabel = "MIGRATION TIME (SEC)"
 
+# 1 2 3 5 8 4 6 7
 # 1 2 3 6 4 7 8 5
 # 1 2 4 8 6 9 10 7
 #cols = c("1", "2", "4", "8", "6", "9", "10", "7" )
@@ -66,8 +67,8 @@ genplot <- function (type) {
 	# convert to one column data
 	#df <- data.frame(values = c(data[1,8], data[1:2,7], data[1:3,6], data[1:4,5], data[1:5,4], data[1:6,3], data[1:7,2], data[1:8,1]), 
 	#df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[4],4], data[1:lens[5],5], data[1:lens[6],6], data[1:lens[7],7], data[1:lens[8],8]), 
-	#df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[4],6], data[1:lens[5],4], data[1:lens[6],7], data[1:lens[7],8], data[1:lens[8],5]), 
-	df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[6],6], data[1:lens[4],4], data[1:lens[7],7], data[1:lens[8],8], data[1:lens[5],5]), 
+	df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[5],5], data[1:lens[8],8], data[1:lens[4],4], data[1:lens[6],6], data[1:lens[7],7]), 
+	#df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[6],6], data[1:lens[4],4], data[1:lens[7],7], data[1:lens[8],8], data[1:lens[5],5]), 
 		vars = rep(cols, times = lens))
 		#vars = rep(c("1", "2", "4", "6", "7", "8"), times = c(1,2,4,6,7,8)))
 	
