214e39cf4c487d4a3855b8d371b63ab6bde084fc
root <root@rapidmigrate.com>
2014-07-03 02:08:32 -0400

    all

--- plot/plotthroughput.R
+++ plot/plotthroughput.R
@@ -37,7 +37,7 @@ data <- data*100/125
 # transpose
 #data <- t(data[,-1]/1000)
 #data <- t(data)
-print(data)
+#print(data)
 
 require(devEMF)
 
@@ -67,25 +67,27 @@ genplot <- function (type) {
 	# convert to one column data
 	#df <- data.frame(values = c(data[1,8], data[1:2,7], data[1:3,6], data[1:4,5], data[1:5,4], data[1:6,3], data[1:7,2], data[1:8,1]), 
 	#df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[4],4], data[1:lens[5],5], data[1:lens[6],6], data[1:lens[7],7], data[1:lens[8],8]), 
-	df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[5],5], data[1:lens[8],8], data[1:lens[4],4], data[1:lens[6],6], data[1:lens[7],7]), 
+#	df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[5],5], data[1:lens[8],8], data[1:lens[4],4], data[1:lens[6],6], data[1:lens[7],7]), 
 	#df <- data.frame(values = c(data[1:lens[1],1], data[1:lens[2],2], data[1:lens[3],3], data[1:lens[6],6], data[1:lens[4],4], data[1:lens[7],7], data[1:lens[8],8], data[1:lens[5],5]), 
-		vars = rep(cols, times = lens))
+		#vars = rep(cols, times = lens))
 		#vars = rep(c("1", "2", "4", "6", "7", "8"), times = c(1,2,4,6,7,8)))
 	
-	print(df)
+#	print(df)
 	
 	ylim <- range(data)
 
 	# las = 2 to rotate xlabels
 	#boxplot(x = 1:nrow(data), y = c(data[1,8], data[1:2,7], data[1:3,6], data[1:4,5], data[1:5,4], data[1:6,3], data[1:7,2], data[1:8,1]),
-	boxplot(values ~ vars, data = df,
+	#boxplot(values ~ vars, data = df,
+	boxplot(data,
 		las = 1, 
 		xlab = xlabel,
 		ylab = ylabel,
 		names = cols,
 		#names = c("1", "2", "4", "6", "7", "8", "9", "10"), 
-#			"9", "10", "12", "14", "16", "18", "20", "30", 
-#			"40", "50", "60", "70", "80", "90", "100"),
+	#	names = c("1", "2", "4", "6", "7", "8", "9", "10", 
+	#		"9", "10", "12", "14", "16", "18", "20", "30", 
+	#		"40", "50", "60", "70", "80", "90", "100"),
 		ylim = c(0, 100)
 		#ylim = c(0, ylim[2])
 	)
