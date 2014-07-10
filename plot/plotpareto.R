c34020c7bcf1c86dfd9ff959e073a9d69c9dc778
root <root@rapidmigrate.com>
2014-07-10 02:19:49 -0400

    all

--- plot/plotpareto.R
+++ plot/plotpareto.R
@@ -78,7 +78,7 @@ genplot <- function (type) {
 	# c(bottom, left, top, right)
 	par(mar = c(5, 5, 1, 1) + 0.1)
 
-	plot(total, total/N, main = "", xlab = xlabel, ylab = ylabel)	
+	plot(total, total/N, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
 	#plot(data, main = "", xlab = xlabel, ylab = ylabel)	
 	#plot(density(data), main = "", xlab = xlabel, ylab = ylabel, xlim = c(0, 100))	
 	#plot(density(data[,1]), main = "", xlab = xlabel, ylab = ylabel)	
