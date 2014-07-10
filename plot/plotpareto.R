24a6cc4d768a52413a3f2931526fea30391ec6d4
root <root@rapidmigrate.com>
2014-07-10 02:51:32 -0400

    all

--- plot/plotpareto.R
+++ plot/plotpareto.R
@@ -79,6 +79,9 @@ genplot <- function (type) {
 	par(mar = c(5, 5, 1, 1) + 0.1)
 
 	plot(total, total/N, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
+	front <- total[c(3,5,9,10,11)]
+	points(front, front/c(4,7,16,18,20), col = "red")
+
 	# pos: 1=below, 2=left, 3=above, 4=right
 	text(total, total/N, c("","",4,"","","","","","","","20"), pos = 2, col = "red")
 	text(total, total/N, c("","","","",7,"","","",16,18,""), pos = 3, col = "red")
