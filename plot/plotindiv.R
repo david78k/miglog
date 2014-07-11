9f2fc70438df9cff6d01a2a8fbc304ee20045156
root <root@rapidmigrate.com>
2014-07-10 22:11:11 -0400

    all

--- plot/plotindiv.R
+++ plot/plotindiv.R
@@ -83,7 +83,9 @@ genplot <- function (type) {
 	# c(bottom, left, top, right)
 	par(mar = c(5, 5, 1, 1) + 0.1)
 
-	plot(N, total, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
+	plot(total, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
+	axis(1, at = 1:12, labels = N)
+	#plot(N, total, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
 	#plot(total, total/N, las = 1, main = "", xlab = xlabel, ylab = ylabel)	
 
 	#front <- total[c(3,5,9,10,11)]
